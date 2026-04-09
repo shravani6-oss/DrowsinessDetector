f2e6bedc6ef7d7bd0d96dd09c030eb366636b731
shravani6-oss <shravanikali6@gmail.com>
2026-04-09 23:26:27 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,6 +3,10 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app
 
-if __name__ == "__main__":
+def main():
     port = int(os.environ.get('PORT', 7860))
     app.run(debug=False, threaded=True, host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    main()
+
